f9b7535dcc95279c3264839c1e4cda7e83eeec7f
Rajeevan S <srajeev2210@gmail.com>
2020-09-13 12:59:45 +0530

    Try Catch Throw - 13 Sept

---
+++ JSChallanges/Object_13Sept.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    let res
+    if(a>0 && b > 0){
+        let peri = 2 * (a + b)
+        let area = a*b
+        res = {
+            length : a,
+            width : b,
+            perimeter : peri,
+            area : area
+        } 
+    }
+    return res
+}
+
+
+function main() {
+    const a = +(readLine());
+    const b = +(readLine());
+    
+    const rec = new Rectangle(a, b);
+    
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+}
