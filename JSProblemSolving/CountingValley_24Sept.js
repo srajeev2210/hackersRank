688378135f3f7a85f063b32bd618b288b2733bc6
Rajeevan S <srajeev2210@gmail.com>
2020-09-24 09:17:19 +0530

    Count valley - 24 Sept

---
+++ JSProblemSolving/CountingValley_24Sept.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    // Write your code here
+    let valley = 0, sealevel = 0
+
+    let s = path.split('')
+    for(let i=0; i<s.length; i++){
+        if(s[i] === 'U'){
+            sealevel += 1
+            if(sealevel == 0){
+                valley += 1 
+            } 
+        } else{
+            sealevel -= 1
+        }
+    }
+    
+    console.log(valley)
+    return valley
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
